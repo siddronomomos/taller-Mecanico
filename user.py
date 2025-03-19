918a7006216e09c90395bf7406a8b99ace0ecc9c
Siddronomomos <christopheralcocer938@gmail.com>
2025-03-18 19:33:33 -0600

    updated | none on user

--- user.py
+++ user.py
@@ -7,35 +7,35 @@ class user:
         self.password = None
         self.perfil = None
 
-    def getID(self) -> int:
+    def getID(self) -> int | None:
         return self.id
     
     def setID(self, id: int) -> bool:
         self.id = id
         return True
 
-    def getNombre(self) -> str:
+    def getNombre(self) -> str | None:
         return self.nombre
     
     def setNombre(self, nombre: str) -> bool:
         self.nombre = nombre
         return True
     
-    def getUserName(self) -> str:
+    def getUserName(self) -> str | None:
         return self.user_name
     
     def setUserName(self, user_name: str) -> bool:
         self.user_name = user_name
         return True
     
-    def getPassword(self) -> str:
+    def getPassword(self) -> str | None:
         return self.password
     
     def setPassword(self, password: str) -> bool:
         self.password = password
         return True
     
-    def getPerfil(self) -> str:
+    def getPerfil(self) -> str | None:
         return self.perfil
     
     def setPerfil(self, perfil: str) -> bool:
